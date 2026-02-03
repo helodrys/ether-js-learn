d3ab745f0fbe274b3a14d22825d261155db60861
helodrys <luxsanachimplee@gmail.com>
2026-02-03 17:20:08 +0700

    initial

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,9 +1,15 @@
 // Require packages
+require("dotenv").config()
+const { ethers } = require("ethers")
 
 // Setup connection
+const URL = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+const provider = new ethers.JsonRpcApiProvider(URL)
 
+const address = "0x388C818CA8B9251b393131C08a736A67ccB19297" 
 async function main() {
   // Get balance
+  const balance = await provider.getBalance()
   // Log balance
 }
 
